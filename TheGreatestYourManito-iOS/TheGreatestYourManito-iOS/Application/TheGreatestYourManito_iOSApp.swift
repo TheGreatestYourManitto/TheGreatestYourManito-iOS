8cd51f4438220aa3c1af854c2ac5f6df5c7f9797
ParkSY0919 <qhr498@gmail.com>
2024-10-23 03:15:26 +0900

    [Hotfix/#1] 탈출성공

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Application/TheGreatestYourManito_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGreatestYourManito_iOSApp.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TheGreatestYourManito_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
