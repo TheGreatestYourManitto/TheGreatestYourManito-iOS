164412378b0de4af8e0a8c31e6d055e02abfe5e9
ParkSY0919 <qhr498@gmail.com>
2024-10-23 03:09:15 +0900

    [Hotfix/#1] Hotfix

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Global/Extension/UIFont+.swift
@@ -0,0 +1,60 @@
+//
+//  UIFont+.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 10/22/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static func pretendardFont(for type: PretendardFont) -> UIFont {
+        return UIFont(name: type.weight, size: type.size) ?? .systemFont(ofSize: type.size)
+    }
+    
+    enum PretendardFont {
+        case heading1, heading2, heading3, heading4, heading5
+        case heading6, subtitle1, subtitle2
+        
+        var size: CGFloat {
+            switch self {
+            case .heading1:
+                38
+            case .heading2:
+                32
+            case .heading3:
+                24
+            case .heading4:
+                20
+            case .heading5:
+                18
+            case .heading6:
+                16
+            case .subtitle1:
+                14
+            case .subtitle2:
+                12
+            }
+        }
+        
+        var weight: String {
+            switch self {
+            case .heading1, .heading2, .heading3, .heading4, .heading5:
+                "Pretendard-Bold"
+            case .heading6, .subtitle1, .subtitle2:
+                "Pretendard-Medium"
+            }
+        }
+        
+        var lineHeight: CGFloat {
+            switch self {
+            case .heading6, .subtitle1, .subtitle2:
+                1.5
+            default: 0
+            }
+        }
+    }
+    
+}
+
