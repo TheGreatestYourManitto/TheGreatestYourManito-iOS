7bd79b47d1c542258ad81be0aced3e843872f19c
ParkSY0919 <qhr498@gmail.com>
2024-10-28 21:33:42 +0900

    [Feat/#10] YMJoinCodeStackView 구현

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/Components/YMJoinCodeStackView.swift
@@ -0,0 +1,52 @@
+//
+//  YMJoinCodeStackView.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 10/28/24.
+//
+
+import SwiftUI
+
+struct YMJoinCodeStackView: View {
+    @State var joinCode: String
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 16) {
+                JoinCodeView(joinCode: $joinCode)
+                
+                HStack(spacing: 10) {
+                    YMCircleButton(circleBtnType: .copy, action: {})
+                    YMCircleButton(circleBtnType: .share, action: {})
+                }
+            }
+        }
+    }
+}
+
+extension YMJoinCodeStackView {
+    
+    struct JoinCodeView: View {
+        @Binding var joinCode: String
+        
+        var body: some View {
+            HStack {
+                Text(joinCode)
+                    .font(.pretendardFont(for: .heading6))
+                    .foregroundStyle(.gray1)
+                    .background(.ymWhite)
+                
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+            .background(.ymWhite)
+            .clipShape(.rect(cornerRadius: 16))
+        }
+    }
+    
+}
+
+#Preview {
+    YMJoinCodeStackView(joinCode: "asdfghjkl")
+}
