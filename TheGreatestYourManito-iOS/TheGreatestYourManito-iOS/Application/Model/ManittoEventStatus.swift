34a9377cb7642ce4842b7e68a52cac5a77f4ce48
FpRaArNkK <mspark1184@naver.com>
2024-10-28 20:52:20 +0900

    [Feat/#9] ManittoEventStatus 작성

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Application/Model/ManittoEventStatus.swift
@@ -0,0 +1,39 @@
+//
+//  ManittoEventStatus.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박민서 on 10/28/24.
+//
+
+import SwiftUI
+
+enum ManittoEventStatus {
+    /// 이벤트 진행 중
+    case ongoing(dDay: Int)
+    /// 이벤트 종료됨
+    case ended
+    
+    var foregroundColor: Color {
+        switch self {
+        case .ongoing: return .gray1
+        case .ended: return .gray1
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .ongoing: return .sub2
+        case .ended: return .gray3
+        }
+    }
+    
+    var labelString: String {
+        switch self {
+            
+        case .ongoing(dDay: let dDay):
+            return dDay == 0 ? "D-Day" : "D-\(dDay)"
+        case .ended:
+            return "종료"
+        }
+    }
+}
