b11ece439ec75a81cf0df124c98f46969a51cf9b
FpRaArNkK <mspark1184@naver.com>
2024-10-28 16:25:25 +0900

    [Feat/#9] playManittoView 베이스 작성

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/Participate/PlayManitto/PlayManittoView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayManittoView.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박민서 on 10/28/24.
+//
+
+import SwiftUI
+// TODO: 커스텀 네비게이션 바 만들기
+
+struct PlayManittoView: View {
+    var body: some View {
+        // Name Tag Comp
+        // Bottom Sheet
+        Text("Temp")
+    }
+}
+
+#Preview {
+    
+}
