748794f145e896df695e65d9d7a7222a60ce51db
ParkSY0919 <qhr498@gmail.com>
2024-11-01 08:27:19 +0900

    [Feat/#10] StringLiterals CreateRoomView_AfterWrite 구현

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Global/Literal/StringLiterals.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Global/Literal/StringLiterals.swift
@@ -7,4 +7,18 @@
 
 import SwiftUI
 
-
+enum StringLiterals {
+    
+    enum CreateRoomView_AfterWrite {
+        static let noticeWordLabel = "참여자가 다 모이면 확인 버튼을 눌러주세요!"
+        static let confirmRoomLabel = "이대로 방을 확정할까요?"
+        static let confirmRoomSubLabel = "확정하면 방 수정 및 삭제가 불가능합니다"
+        static let removeMemberLabel = "님을 삭제할까요?"
+        static let removeMemberSubLabel = "지금 삭제해도 다시 초대할 수 있어요!"
+        static let deleteBottomSheetLeftBtnLabel = "아니요"
+        static let deleteBottomSheetRightBtnLabel = "삭제"
+        static let confirmBottomSheetLeftBtnLabel = "취소"
+        static let confirmBottomSheetRightBtnLabel = "확정"
+    }
+    
+}
