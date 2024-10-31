0a049bd5898ae47f62e713eccda19a1c8b6d1d8a
ParkSY0919 <qhr498@gmail.com>
2024-10-31 23:43:13 +0900

    [Feat/#10] YMBottomSheetModifier 구현 등
    CreateRoom_AfterWriteViewModel 추가

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/CreateRoom(After-Write)/CreateRoom_AfterWriteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CreateRoom_AfterWriteViewModel.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 10/29/24.
+//
+
+import SwiftUI
+
+struct JoinMemberModel {
+    var memberName: String
+}
+
