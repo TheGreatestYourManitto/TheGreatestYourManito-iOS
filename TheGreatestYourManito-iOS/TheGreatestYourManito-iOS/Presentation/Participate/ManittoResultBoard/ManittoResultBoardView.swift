c92711cf8f958bd81042ea2d01dab92bd6c90c86
FpRaArNkK <mspark1184@naver.com>
2024-11-09 13:17:28 +0900

    [Feat/#17] ManuttoResultBoardView init

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/Participate/ManittoResultBoard/ManittoResultBoardView.swift
@@ -0,0 +1,14 @@
+//
+//  ManittoResultView.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박민서 on 11/9/24.
+//
+
+import SwiftUI
+
+struct ManittoResultBoardView: View {
+    var body: some View {
+        Text("ManittoResultBoardView")
+    }
+}
