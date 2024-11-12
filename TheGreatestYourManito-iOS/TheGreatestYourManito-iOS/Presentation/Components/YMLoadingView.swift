a1fa1f9a40200d9f44e2805d0d79c0596e681296
ParkSY0919 <qhr498@gmail.com>
2024-11-13 04:45:55 +0900

    [Feat/#14] YMCheerImage 사용 로딩뷰 구현

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/Components/YMLoadingView.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/Components/YMLoadingView.swift
@@ -5,4 +5,44 @@
 //  Created by 박신영 on 11/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct YMLoadingView: View {
+    @State private var visibleCount = 0
+    private let animationInterval = 0.4
+    let titleText: String
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.7)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                Text(titleText)
+                    .font(.pretendardFont(for: .heading4))
+                    .foregroundColor(.ymWhite)
+                HStack(spacing: 16) {
+                    ForEach(CheerType.allCases.indices, id: \.self) { index in
+                        Image(CheerType.allCases[index].defaultImage)
+                            .opacity(index < visibleCount ? 1 : 0)
+                            .animation(.easeInOut(duration: animationInterval), value: visibleCount)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            startAnimation()
+        }
+    }
+    
+    private func startAnimation() {
+        Timer.scheduledTimer(withTimeInterval: animationInterval, repeats: true) { timer in
+            visibleCount += 1
+            
+            if visibleCount > CheerType.allCases.count {
+                visibleCount = 0 // 모든 이미지가 표시된 이후 모두 제거
+            }
+        }
+    }
+}
+
