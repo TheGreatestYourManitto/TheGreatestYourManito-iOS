73b917822c22a795a03424bc92a94395a14c520b
ParkSY0919 <qhr498@gmail.com>
2024-11-13 00:53:35 +0900

    [Fix/#14] StringLiterals 수정

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Global/Literal/StringLiterals.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Global/Literal/StringLiterals.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 enum StringLiterals {
     
-    enum CreateRoomView_AfterWrite {
-        static let noticeWordLabel = "참여자가 다 모이면 확인 버튼을 눌러주세요!"
+    enum AfterJoinRoom {
+        static let ownerNoticeWordLabel = "참여자가 다 모이면 확인 버튼을 눌러주세요!"
+        static let notOwnerNoticeWordLabel = "참여자를 기다리고있어요~"
         static let confirmRoomLabel = "이대로 방을 확정할까요?"
         static let confirmRoomSubLabel = "확정하면 방 수정 및 삭제가 불가능합니다"
         static let removeMemberLabel = "님을 삭제할까요?"
@@ -21,13 +22,10 @@ enum StringLiterals {
         static let confirmBottomSheetRightBtnLabel = "확정"
     }
     
-    enum JoinRoom_After {
-        static let noticeWordLabel = "참여자를 기다리고있어요~"
-    }
-    
     enum JoinRoom_Before {
         static let titleLabel = "방 입장하기"
-        static let subTitleLabel = "두근두근\n나만의 마니또를 만나러 - !"
+        static let subTitleLabel1 = "두근두근"
+        static let subTitleLabel2 = "나만의 마니또를 만나러 - !"
         static let bottomSheetTitleLabel = "방 참여코드"
         static let bottomSheetSubTitleLabel = "방장에게 받은 참여 코드를 입력해주세요"
         static let bottomSheetPlaceholder = "참여 코드"
