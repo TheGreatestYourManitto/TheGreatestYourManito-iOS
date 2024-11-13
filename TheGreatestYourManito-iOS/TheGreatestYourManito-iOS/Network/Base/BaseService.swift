b3456e1bd8ba994633809cb496c065b1f651a656
ParkSY0919 <qhr498@gmail.com>
2024-11-14 05:23:31 +0900

    [Feat/#24] BaseService

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Base/BaseService.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Base/BaseService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class BaseService {
+   func judgeStatus<T: Codable>(statusCode: Int, data: Data) -> NetworkResult<T> {
+      switch statusCode {
+      case 200..<205, 401:
+         return isValidData(data: data, responseType: T.self)
+      case 400, 402..<500:
+         return .requestErr
+      case 500:
+         return .serverErr
+      default:
+         return .networkFail
+      }
+   }
+   
+   func isValidData<T: Codable>(data: Data, responseType: T.Type) -> NetworkResult<T> {
+      let decoder = JSONDecoder()
+      guard let decodedData = try? decoder.decode(T.self, from: data) else {
+         print("⛔️ \(self)애서 디코딩 오류가 발생했습니다 ⛔️")
+         return .decodedErr
+      }
+      
+      return .success(decodedData)
+   }
+}
+
