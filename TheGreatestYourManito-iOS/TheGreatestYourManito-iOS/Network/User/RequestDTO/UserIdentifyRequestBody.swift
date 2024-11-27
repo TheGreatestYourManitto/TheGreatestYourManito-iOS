525c8a65c75d03a2a4bc3b05b8f05eab904ef54f
ParkSY0919 <qhr498@gmail.com>
2024-11-27 22:06:38 +0900

    [Feat/#30] UserIdentifyRequestBody

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/User/RequestDTO/UserIdentifyRequestBody.swift
@@ -0,0 +1,12 @@
+//
+//  UserIdentifyRequestBody.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 11/27/24.
+//
+
+import Foundation
+
+struct UserIdentifyRequestBody {
+    let deviceId: String
+}
