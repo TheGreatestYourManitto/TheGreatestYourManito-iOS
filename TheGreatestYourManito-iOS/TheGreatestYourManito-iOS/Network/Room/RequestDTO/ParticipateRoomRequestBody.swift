76050b0f03d7348b42ca311183949b469fa5d804
ParkSY0919 <qhr498@gmail.com>
2024-11-28 05:08:47 +0900

    [Feat/#33] ParticipateRoomRequestBody

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/RequestDTO/ParticipateRoomRequestBody.swift
@@ -0,0 +1,12 @@
+//
+//  ParticipateRoomRequestBody.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 11/28/24.
+//
+
+import Foundation
+
+struct ParticipateRoomRequestBody: Encodable {
+    let invitationCode: String
+}
