9c733d2f1e5e1e42a4b0c35b113720ba5a0686ec
ParkSY0919 <qhr498@gmail.com>
2024-11-28 00:23:07 +0900

    [Feat/#30] postUserIdentify() 구현

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/User/UserService.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/User/UserService.swift
@@ -11,7 +11,7 @@ import Moya
 
 protocol UserServiceProtocol {
     
-//    func postUserIdentify(completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ())
+    func postUserIdentify(requestBody: UserIdentifyRequestBody, completion: @escaping (NetworkResult<BaseResponseBody>) -> ())
     
     func postMakeUser(requestBody: MakeUserRequestBody, completion: @escaping (NetworkResult<BaseResponseBody>) -> ())
     
@@ -20,6 +20,18 @@ protocol UserServiceProtocol {
 final class UserService: BaseService, UserServiceProtocol {
     
     let provider = MoyaProvider<UserTargetType>(plugins: [MoyaLoggingPlugin()])
+    
+    func postUserIdentify(requestBody: UserIdentifyRequestBody, completion: @escaping (NetworkResult<BaseResponseBody>) -> ()) {
+        provider.request(.postUserIdentify(requestBody: requestBody)) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<BaseResponseBody> = self.judgeStatus(statusCode: response.statusCode, data: response.data)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
     
     func postMakeUser(requestBody: MakeUserRequestBody, completion: @escaping (NetworkResult<BaseResponseBody>) -> ()) {
         provider.request(.postMakeUser(requestBody: requestBody)) { result in
@@ -35,15 +47,14 @@ final class UserService: BaseService, UserServiceProtocol {
     
 }
 
-
+//MARK: - PostMakeUser
 /// 1. postMakeUser 사용할 때에 아래 함수 해당 뷰 ViewModel에 추가
 /// 2. 위 ViewModel의 View에 .onappear 코드 사용
 
 /*
- 1.
+//PostMakeUser
 func postMakeUser() {
-    let device = Device()
-    let udid = device.getDeviceUUID()
+ guard let deviceId = UserDefaults.standard.string(forKey: "deviceId") else {return}
     
     let requestBody = MakeUserRequestBody(nickname: "psy", deviceId: udid)
     NetworkService.shared.userService.postMakeUser(requestBody: requestBody, completion: { result in
@@ -56,9 +67,30 @@ func postMakeUser() {
         }
     })
 }
-
- 2.
+ 
+...
+ 
 .onAppear(perform: {
    (뷰 모델 변수명).postMakeUser()
 })
 */
+
+
+//MARK: - PostUserIdentify
+/**
+func postUserIdentify() {
+ let device = Device()
+ let udid = device.getDeviceUUID()
+ guard let userDefaults = UserDefaults.standard.string(forKey: "deviceId") else {return}
+ let requestBody = UserIdentifyRequestBody(deviceId: userDefaults)
+ NetworkService.shared.userService.postUserIdentify(requestBody: requestBody, completion: { result in
+     switch result {
+     case .success(let response):
+         print("Success: \(response)")
+     default:
+         print("Failed to another reason")
+         return
+     }
+ })
+}
+ */
