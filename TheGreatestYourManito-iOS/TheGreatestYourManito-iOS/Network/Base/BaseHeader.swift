2a399fa6bf8867f8cbdb258312c11fe857e3d3dc
ParkSY0919 <qhr498@gmail.com>
2024-11-28 05:20:57 +0900

    [Feat/#33] BaseHeader 구현

---
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Base/BaseHeader.swift
@@ -0,0 +1,17 @@
+//
+//  BaseHeader.swift
+//  TheGreatestYourManito-iOS
+//
+//  Created by 박신영 on 11/28/24.
+//
+
+import Foundation
+
+struct BaseHeader {
+    
+    static func headerWithUserCode() -> [String: String] {
+        guard let userCode = UserDefaults.standard.string(forKey: "userCode") else {return ["": ""]}
+        return ["userCode": userCode]
+    }
+    
+}
