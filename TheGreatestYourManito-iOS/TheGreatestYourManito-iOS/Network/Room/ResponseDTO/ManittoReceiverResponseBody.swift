ed9fc47072e120adecdd0e844d8f6d54d67361ba
ParkSY0919 <qhr498@gmail.com>
2024-12-03 06:08:04 +0900

    [Feat/#39] feat ManittoReceiverResponseBody

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/ManittoReceiverResponseBody.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/ManittoReceiverResponseBody.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+// MARK: - ManittoReceiverResponseBody
+struct ManittoReceiverResponseBody: Codable {
+    let receiver: Receiver
+    let endDate: String
+}
+
+// MARK: - Receiver
+struct Receiver: Codable {
+    let userName: String
+    let userId: Int
+}
