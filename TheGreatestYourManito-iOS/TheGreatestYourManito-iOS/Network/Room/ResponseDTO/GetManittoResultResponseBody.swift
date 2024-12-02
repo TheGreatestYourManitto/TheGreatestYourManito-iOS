43e58cf4d8d8fc04c02eac4ff98c93ee8c91afaa
ParkSY0919 <qhr498@gmail.com>
2024-12-03 06:36:17 +0900

    [Feat/#39] feat GetManittoResultResponseBody

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/GetManittoResultResponseBody.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/GetManittoResultResponseBody.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+// MARK: - GetManittoResultResponseBody
+struct GetManittoResultResponseBody: Codable {
+    let manitto: Manitto
+    let cheerCounts: [CheerCount]
+    let manittoRank: [ManittoRank]
+}
+
+// MARK: - CheerCount
+struct CheerCount: Codable {
+    let type: String
+    let count: Int
+}
+
+// MARK: - Manitto
+struct Manitto: Codable {
+    let userName: String
+    let userId: Int
+}
+
+// MARK: - ManittoRank
+struct ManittoRank: Codable {
+    let rank, userId: Int
+    let userName: String
+    let manittoUserId: Int
+    let manittoUserName: String
+    let cheerCount: Int
+}
+
