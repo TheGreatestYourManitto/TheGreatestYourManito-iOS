77457e5d005895ddb43acaf56bef2b448b0b2339
ParkSY0919 <qhr498@gmail.com>
2024-12-03 07:06:39 +0900

    [Feat/#41] getCheerMassage API 구현

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Cheer/CheerTargetType.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Cheer/CheerTargetType.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+import Moya
+
+enum CheerTargetType {
+    case getCheerMessage(type: String)
+}
+
+extension CheerTargetType: BaseTargetType {
+    
+    var method: Moya.Method {
+        switch self {
+        case .getCheerMessage:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getCheerMessage(let type):
+            return "/room/\(type)/message"
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getCheerMessage:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .getCheerMessage:
+            return nil
+        default:
+            return BaseHeader.headerWithUserCode()
+        }
+        
+    }
+    
+}
+
+
