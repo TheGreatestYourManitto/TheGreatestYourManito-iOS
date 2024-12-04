49e00431dc782ae6f205466f8909fb881fafafb4
jaminleee <jamin_00@naver.com>
2024-12-04 21:27:01 +0900

    [Feat/#44] CreateRoomVM dDay계산 로직 추가

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/CreateRoom/CreateRoomViewModel.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Presentation/CreateRoom/CreateRoomViewModel.swift
@@ -16,8 +16,10 @@ final class CreateRoomViewModel: ObservableObject {
     @Published var joinCode: String = ""
     @Published var dDay: Int = 0
     
-    func updateDateTime() {
-        guard let date = selectedDate, let time = selectedTime else { return }
+    func updateDateTime(date: Date?, time: Date?) {
+        let date = selectedDate ?? Date()
+        let time = selectedTime ?? Date()
+        
         let calendar = Calendar.current
         let dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
         let timeComponents = calendar.dateComponents([.hour, .minute], from: time)
@@ -27,15 +29,27 @@ final class CreateRoomViewModel: ObservableObject {
         combinedComponents.day = dateComponents.day
         combinedComponents.hour = timeComponents.hour
         combinedComponents.minute = timeComponents.minute
-
+        
         endDateTime = calendar.date(from: combinedComponents)
+        
+        
     }
+    
+    func calculateDday(date: Date?) {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date()) // 오늘의 날짜를 00:00으로 설정
+        let targetDate = calendar.startOfDay(for: date ?? Date()) // 선택된 날짜를 00:00으로 설정
+        
+        // 날짜 차이를 계산
+        dDay = calendar.dateComponents([.day], from: today, to: targetDate).day ?? 0
+    }
+
 }
 
 extension CreateRoomViewModel {
     
     func postMakeRoom(onCompletion: @escaping () -> Void) {
-        updateDateTime()
+        updateDateTime(date: selectedDate, time: selectedTime)
         guard let endDateTime = endDateTime else { return print("endDateTime error")}
         print("roomName: \(roomName), endDateTime: \(endDateTime.toISO8601String()) 전송 API 요청")
         let requestBody = MakeRoomRequestBody(roomName: roomName, endDate: endDateTime.toISO8601String())
