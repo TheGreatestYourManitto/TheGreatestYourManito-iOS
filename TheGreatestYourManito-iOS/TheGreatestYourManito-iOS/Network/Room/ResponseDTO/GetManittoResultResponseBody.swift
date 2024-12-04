784e0c1617e46fc37318fe523e1f75910ff74ec7
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2024-12-04 16:37:14 +0900

    [Feat/#34] GetManittoResultResponse mapper 추가

--- TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/GetManittoResultResponseBody.swift
+++ TheGreatestYourManito-iOS/TheGreatestYourManito-iOS/Network/Room/ResponseDTO/GetManittoResultResponseBody.swift
@@ -20,6 +20,22 @@ struct CheerCount: Codable {
     let count: Int
 }
 
+typealias CheerCounts = [CheerCount]
+
+extension CheerCounts {
+    func toCheerCountDic() -> [CheerType: Int] {
+        self.compactMap { cheerCount -> (CheerType, Int)? in
+            if let cheerType = CheerType(name: cheerCount.type) {
+                return (cheerType, cheerCount.count)
+            } else {
+                return nil
+            }
+        }.reduce(into: [CheerType: Int]()) { result, pair in
+            result[pair.0] = pair.1
+        }
+    }
+}
+
 // MARK: - Manitto
 struct Manitto: Codable {
     let userName: String
@@ -33,5 +49,20 @@ struct ManittoRank: Codable {
     let manittoUserId: Int
     let manittoUserName: String
     let cheerCount: Int
+}
+
+typealias ManittoRanks = [ManittoRank]
+
+extension ManittoRanks {
+    func toManittoRankList() -> ManittoRankList {
+        self.map {
+            ManittoRankItem(
+                rank: ManiitoRank(rank: $0.rank),
+                fromPerson: User(id: $0.manittoUserId, name: $0.manittoUserName),
+                toPerson: User(id: $0.userId, name: $0.userName),
+                cheerCount: $0.cheerCount
+            )
+        }
+    }
 }
 
